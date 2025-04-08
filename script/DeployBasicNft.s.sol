6019b602d1ca09e4fadda6b8dcf7171bba31613f
saikrishnacret <saikrishnaask191@gmail.com>
2025-04-08 22:50:34 +0530

    DNft

--- script/DeployBasicNft.s.sol
+++ script/DeployBasicNft.s.sol
@@ -5,7 +5,6 @@ import {Script} from "forge-std/Script.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 
 contract DeployBasicNft is Script {
-
     function run() external returns(BasicNft){
         vm.startBroadcast();
         BasicNft basicNft = new BasicNft();
